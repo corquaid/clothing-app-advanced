0e778905597620b621f71a43c6bd547918b8b043
Cormac Quaid <thequad@gmail.com>
2021-08-31 09:43:02 +0200

    Initial commit having set up Github + Heroku configuration

--- client/src/redux/directory/directory.selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectDirectory = state => state.directory;
-
-// this selector will return the desired directory sections
-export const selectDirectorySections = createSelector(
-  [selectDirectory],
-  directory => directory.sections
-)
--- client/src/redux/shop/shop.types.js
+++
@@ -1,7 +0,0 @@
-const ShopActionTypes = {
-  FETCH_COLLECTIONS_START: 'FETCH_COLLECTIONS_START', // when first API call to Firestore 
-  FETCH_COLLECTIONS_SUCCESS: 'FETCH_COLLECTIONS_SUCCESS', // when API call returns successfully
-  FETCH_COLLECTIONS_FAILURE: 'FETCH_COLLECTIONS_FAILURE'
-};
-
-export default ShopActionTypes;
